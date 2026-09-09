9427ebfe3c726a6e06eedebb3b1852a813822428
Sun <wanglt-123@qq.com>
2026-09-09 17:24:09 +0800

    feat(gateway): 集成配置模块并添加静态文件服务支持
    - 配置ConfigModule为全局模块，支持环境变量扩展和多环境配置文件加载
    - 添加@fastify/static依赖以支持静态文件服务功能
    - 集成项目配置文件到应用模块中

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import config from '../../../packages/config/src/index.js';
 import { AppController } from './app.controller.js';
+
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env.local', `.env.${process.env.NODE_ENV}`, '.env'],
+      load: [...Object.values(config)],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
